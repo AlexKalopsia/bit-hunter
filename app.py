6309d7664d0cadc93c8078e2db6ce10bb42d09c5
Alex Camilleri <camilleri.alex@gmail.com>
2020-05-28 16:47:48 +0200

    Scrapes trophie images from PSNProfiles

--- app.py
+++ app.py
@@ -1,18 +1,50 @@
 from PIL import Image
 from PIL import ImageDraw
+from bs4 import BeautifulSoup
+from io import BytesIO
+import requests
+import sys
 
-imgFrame = Image.open('./images/frame.png')
-imgFrame_size = imgFrame.size
+# Trophies scraping
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0"}
+
+URL = "https://psnprofiles.com/trophies/10903-atomicrops"
+page = requests.get(URL, headers=headers)
+soup = BeautifulSoup(page.content, 'html.parser')
+images = soup.find_all("picture", class_="trophy")
+
+urlImages = []
+
+for image in images:
+    info = str(image)
+    urlStart = info.find('<img src="')+10
+    url_1 = info[urlStart:]
+    urlEnd = url_1.find('"')
+    #urlEnd = info.find('"></img></picture>')
+    urlImage = url_1[:urlEnd]
+
+    print(urlImage)
+    urlImages.append(urlImage)
+
+for urlImage in urlImages:
+    response = requests.get(urlImage)
+
+# Image processing
 
 
-imgTrophy = Image.open('./images/trophy.png')
-imgTrophy_size = imgTrophy.size
+def ProcessImage(_image):
+    imgFrame = Image.open('./images/frame.png')
+    imgFrame_size = imgFrame.size
 
-imgTrophy_s = imgTrophy.resize((210, 210))
+    imgTrophy = Image.open('./images/trophy.png')
+    imgTrophy_size = imgTrophy.size
 
-imgFinal = Image.new('RGB', imgFrame_size, color='#fff')
-imgFinal.paste(imgFrame)
-imgFinal.paste(imgTrophy_s, (15, 15))
+    imgTrophy_s = imgTrophy.resize((210, 210))
 
-imgFinal.show()
-#final = imgFinal.save('.images/')
+    imgFinal = Image.new('RGB', imgFrame_size, color='#fff')
+    imgFinal.paste(imgFrame)
+    imgFinal.paste(imgTrophy_s, (15, 15))
+
+    # imgFinal.show()
+    #final = imgFinal.save('.images/')
